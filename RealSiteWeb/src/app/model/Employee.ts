140205770b58dc021a7e91235fce2c3306814026
nathmakuru <61262795+nathmakuru@users.noreply.github.com>
2025-09-03 23:36:23 +0530

    realsite ver 2

--- RealSiteWeb/src/app/model/Employee.ts
+++ RealSiteWeb/src/app/model/Employee.ts
@@ -3,6 +3,4 @@ export interface IEmployee {
   employee_name?: string; // optional, max length 200 (can be validated in Angular)
   employee_phone?: string;
   employee_email?: string;
-  employee_password?: string;
-  verification_code?: string;
 }
